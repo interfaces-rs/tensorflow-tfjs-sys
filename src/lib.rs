9d59a619145f7472560c821894f45f7ad082d8ef
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-08 14:46:04 -0700

    Add type for Tensor

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+use js_sys::{Array, Promise};
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(module = "@tensorflow/tfjs")]
+extern {
+    /// A Tensor object represents an immutable, multidimensional array of numbers that has a
+    /// shape and a data type.
+    #[wasm_bindgen(extends = Promise)]
+    pub type Tensor;
+
+}
