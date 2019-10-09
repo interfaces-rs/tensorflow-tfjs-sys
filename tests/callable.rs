fd88b69f7e2d14bcb7f628164950d65809826fc5
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-09 10:55:06 -0700

    Move tensor tests into submodule
    [skip ci]

--- tests/callable.rs
+++ tests/callable.rs
@@ -1,28 +1,30 @@
-use tfjs_sys as tf;
-use wasm_bindgen_test::*;
+mod tensor {
+    use tfjs_sys as tf;
+    use wasm_bindgen_test::*;
 
-#[wasm_bindgen_test]
-async fn print() {
-    let value: u32 = Default::default();
-    let dtype = Default::default();
-    let tensor = tf::scalar(&value.into(), dtype);
-    tensor.print(false);
-}
+    #[wasm_bindgen_test]
+    async fn print() {
+        let value: u32 = Default::default();
+        let dtype = Default::default();
+        let tensor = tf::scalar(&value.into(), dtype);
+        tensor.print(false);
+    }
 
-#[wasm_bindgen_test]
-async fn scalar() {
-    let value: u32 = Default::default();
-    let dtype = Default::default();
-    let tensor = tf::scalar(&value.into(), dtype);
-    tensor.print(false);
-}
+    #[wasm_bindgen_test]
+    async fn scalar() {
+        let value: u32 = Default::default();
+        let dtype = Default::default();
+        let tensor = tf::scalar(&value.into(), dtype);
+        tensor.print(false);
+    }
 
-#[wasm_bindgen_test]
-async fn tensor() {
-    use js_sys::Array;
-    let values = Array::new();
-    let shape = Default::default();
-    let dtype = Default::default();
-    let tensor = tf::tensor(&values, shape, dtype);
-    tensor.print(false);
+    #[wasm_bindgen_test]
+    async fn tensor() {
+        use js_sys::Array;
+        let values = Array::new();
+        let shape = Default::default();
+        let dtype = Default::default();
+        let tensor = tf::tensor(&values, shape, dtype);
+        tensor.print(false);
+    }
 }
