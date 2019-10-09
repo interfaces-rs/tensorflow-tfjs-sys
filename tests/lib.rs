9e49ac02c42693d16d7b70748be4132eda8048b0
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-09 10:11:44 -0700

    Rearrange imports

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,9 @@
-use js_sys::Array;
 use tfjs_sys as tf;
 use wasm_bindgen_test::*;
 
 #[wasm_bindgen_test]
 async fn tensor_callable() {
+    use js_sys::Array;
     let values = Array::new();
     let shape = Default::default();
     let dtype = Default::default();
