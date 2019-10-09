cf2361e15e3290c6acec8df9c171ed93634a27f3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-09 10:48:03 -0700

    Don't extend Promise
    [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use js_sys::{Array, Promise};
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "@tensorflow/tfjs")]
 extern {
     /// A Tensor object represents an immutable, multidimensional array of numbers that has a
     /// shape and a data type.
-    #[wasm_bindgen(extends = Promise)]
     pub type Tensor;
 
     /// Prints information about the Tensor including its data.
