b36401cd0aee7e132f3d4897bfc727e7bf4216c4
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-10 16:47:42 -0700

    Start filling out modules
    [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+mod constraints;
+mod data;
+mod initializers;
+mod layers;
+mod models;
 mod tensors;
+mod training;
 
+pub use constraints::*;
+pub use data::*;
+pub use initializers::*;
+pub use layers::*;
+pub use models::*;
 pub use tensors::*;
+pub use training::*;
