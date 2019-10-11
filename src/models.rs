539c3367d2e5b4c3590ba158cdf7946f11c99013
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-10 17:46:55 -0700

    Add bindings for LayersModel

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
-use js_sys::{Object, Promise};
+use crate::{data::Dataset, layers::Layer};
+use js_sys::{Function, JsString, Object, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "@tensorflow/tfjs")]
@@ -23,4 +24,61 @@ extern {
     /// Releases the memory used by the weight tensors.
     #[wasm_bindgen(method)]
     pub fn dispose(this: &GraphModel);
+}
+
+#[wasm_bindgen(module = "@tensorflow/tfjs")]
+extern {
+    /// A LayersModel is a directed, acyclic graph of Layers plus methods for training,
+    /// evaluation, prediction and saving.
+    pub type LayersModel;
+
+    /// Configures and prepares the model for training and evaluation. Compiling outfits the model
+    /// with an optimizer, loss, and/or metrics. Calling fit or evaluate on an un-compiled model
+    /// will throw an error.
+    #[wasm_bindgen(method)]
+    pub fn compile(this: &LayersModel, args: &Object);
+
+    /// Returns the loss value & metrics values for the model in test mode.
+    #[wasm_bindgen(method)]
+    pub fn evaluate(this: &LayersModel, x: &JsValue, y: &JsValue, args: Option<&Object>) -> JsValue; // FIXME
+
+    /// Evaluate model using a dataset object.
+    #[wasm_bindgen(method, js_name = "evaluateDataset")]
+    #[must_use]
+    pub fn evaluate_dataset(this: &LayersModel, dataset: &Dataset, args: Option<&Object>) -> Promise;
+
+    /// Trains the model for a fixed number of epochs (iterations on a dataset).
+    #[wasm_bindgen(method)]
+    #[must_use]
+    pub fn fit(this: &LayersModel, x: &JsValue, y: &JsValue, args: Option<&Object>) -> Promise;
+
+    /// Trains the model using a dataset object.
+    #[wasm_bindgen(method, js_name = "fitDataset")]
+    #[must_use]
+    pub fn fit_dataset(this: &LayersModel, dataset: &Dataset, args: &Object) -> Promise;
+
+    /// Retrieves a layer based on either its name (unique) or index.
+    #[wasm_bindgen(method, js_name = "getLayer")]
+    pub fn get_layer(this: &LayersModel, name: Option<&JsString>, index: Option<usize>) -> Layer;
+
+    /// Generates output predictions for the input samples.
+    #[wasm_bindgen(method)]
+    pub fn predict(this: &LayersModel, x: &JsValue, args: Option<&Object>) -> JsValue;
+
+    /// Returns predictions for a single batch of samples.
+    #[wasm_bindgen(method, js_name = "predictOnBatch")]
+    pub fn predict_on_batch(this: &LayersModel, x: &JsValue) -> JsValue;
+
+    /// Save the configuration and/or weights of the LayersModel.
+    #[wasm_bindgen(method)]
+    #[must_use]
+    pub fn save(this: &LayersModel, url: &JsString, config: Option<&Object>) -> Promise;
+
+    /// Print a text summary of the model's layers.
+    #[wasm_bindgen(method)]
+    pub fn summary(this: &LayersModel, line_length: Option<u32>, position: Option<&[u32]>, print_fn: &Function);
+
+    /// Runs a single gradient update on a single batch of data.
+    #[wasm_bindgen(method, js_name = "trainOnBatch")]
+    pub fn train_on_batch(this: &LayersModel, x: &JsValue, y: &JsValue) -> Promise;
 }
