962a9f7ee9b6dc9b17096b9c5a9bda898408b683
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-13 16:21:27 -0700

    Add Initializer

--- src/initializers.rs
+++ src/initializers.rs
@@ -1,1 +1,9 @@
+use crate::Serializable;
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern {
+    /// Initializer base class.
+    #[wasm_bindgen(extends = Serializable)]
+    pub type Initializer;
+}
