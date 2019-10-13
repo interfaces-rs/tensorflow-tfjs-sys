99afa18db9f36eeec4ef9db424368f828b2fb9f7
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-13 16:17:50 -0700

    Add Constraint
    [skip ci]

--- src/constraints.rs
+++ src/constraints.rs
@@ -1,1 +1,10 @@
 
+use crate::Serializable;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    /// Base class for functions that impose constraints on weight values.
+    #[wasm_bindgen(extends = Serializable)]
+    pub type Constraint;
+}
