6cfdd7d884859e00f1b846f8ef0c63884616f050
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-13 14:00:57 -0700

    Add DisposeResult

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,18 @@ pub use layers::*;
 pub use models::*;
 pub use tensors::*;
 pub use training::*;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub type DisposeResult;
+
+    /// Reference count after the dispose call.
+    #[wasm_bindgen(method, getter, js_name = "refCountAfterDispose")]
+    fn ref_count_after_dispose(this: &DisposeResult) -> usize;
+
+    /// Number of variables dispose in this dispose call.
+    #[wasm_bindgen(method, getter, js_name = "numDisposedVariables")]
+    fn num_disposed_variables(this: &DisposeResult) -> usize;
+}
