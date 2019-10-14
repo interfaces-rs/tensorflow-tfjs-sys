70af9c4d59314592fd8f4b25776343f1d1f9c679
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-14 16:00:08 -0700

    Bindings for callback functions

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,1 +1,10 @@
+use js_sys::{Function, Object};
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(module = "@tensorflow/tfjs")]
+extern {
+    /// Factory function for a Callback that stops training when a monitored quantity has stopped
+    /// improving.
+    #[wasm_bindgen(js_name = "earlyStopping")]
+    pub fn early_stopping(args: Option<&Object>) -> Function;
+}
