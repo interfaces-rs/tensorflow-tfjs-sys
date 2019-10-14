f670e7ea6b94a7dea740f3a51a81e803c51dcd77
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-14 10:33:59 -0700

    Add methods for Dataset

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,4 @@
-use js_sys::Object;
+use js_sys::{Function, Object, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "@tensorflow/tfjs")]
@@ -17,4 +17,53 @@ extern {
     /// Represents a potentially large list of independent data elements (typically 'samples' or
     /// 'examples').
     pub type Dataset;
+
+    /// Groups elements into batches.
+    #[wasm_bindgen(method)]
+    pub fn batch(this: &Dataset, batch_size: usize, small_last_batch: Option<bool>) -> Dataset;
+
+    /// Concatenates this Dataset with another.
+    #[wasm_bindgen(method)]
+    pub fn concatenate(this: &Dataset, that: &Dataset) -> Dataset;
+
+    /// Filters this dataset according to predicate.
+    #[wasm_bindgen(method)]
+    pub fn filter(this: &Dataset, predicate: &Function) -> Dataset;
+
+    /// Apply a function to every element of the dataset.
+    #[wasm_bindgen(method, js_name = "forEachAsync")]
+    pub fn for_each_async(this: &Dataset, transform: &Function) -> Promise;
+
+    /// Maps this dataset through a 1-to-1 transform.
+    #[wasm_bindgen(method)]
+    pub fn map(this: &Dataset, transform: &Function) -> Dataset;
+
+    /// Maps this dataset through an async 1-to-1 transform.
+    #[wasm_bindgen(method, js_name = "mapAsync")]
+    pub fn map_async(this: &Dataset, transform: &Function) -> Dataset;
+
+    /// Creates a Dataset that prefetches elements from this dataset.
+    #[wasm_bindgen(method)]
+    pub fn prefetch(this: &Dataset, buffer_size: usize) -> Dataset;
+
+    /// Repeats this dataset count times.
+    #[wasm_bindgen(method)]
+    pub fn repeat(this: &Dataset, count: Option<usize>) -> Dataset;
+
+    /// Creates a Dataset that skips count initial elements from this dataset.
+    #[wasm_bindgen(method)]
+    pub fn skip(this: &Dataset, count: Option<usize>) -> Dataset;
+
+    /// Pseudorandomly shuffles the elements of this dataset. This is done in a streaming manner, by
+    /// sampling from a given number of prefetched elements.
+    #[wasm_bindgen(method)]
+    pub fn shuffle(this: &Dataset, buffer_size: usize, seed: Option<&str>, reshuffle_each_iteration: Option<bool>) -> Dataset;
+
+    /// Creates a Dataset with at most count initial elements from this dataset.
+    #[wasm_bindgen(method)]
+    pub fn take(this: &Dataset, count: Option<usize>) -> Dataset;
+
+    /// Collect all elements of this dataset into an array.
+    #[wasm_bindgen(method, js_name = "toArray")]
+    pub fn to_array(this: &Dataset) -> Promise;
 }
