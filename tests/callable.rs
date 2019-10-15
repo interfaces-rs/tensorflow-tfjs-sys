06dabdca12058f8278e9b09a43f4bd286c5e854d
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-10-15 16:25:40 -0700

    Separate method and function tests

--- tests/callable.rs
+++ tests/callable.rs
@@ -10,277 +10,286 @@ mod helper {
 }
 
 mod tensor {
-    use js_sys::Array;
-    use tfjs_sys as tf;
-    use wasm_bindgen_test::*;
+    mod functions {
+        use js_sys::Array;
+        use super::super::helper;
+        use tfjs_sys as tf;
+        use wasm_bindgen_test::*;
 
-    #[wasm_bindgen_test]
-    fn as_1d() {
-        let value = Array::new();
-        let dtype = Default::default();
-        let tensor = tf::tensor1d(&value.into(), dtype);
-        tensor.as_1d();
-    }
+        #[wasm_bindgen_test]
+        fn buffer() {
+            let shape: &[i32] = &[];
+            let dtype = Default::default();
+            let values = Default::default();
+            tf::buffer(&shape, dtype, values);
+        }
 
-    #[wasm_bindgen_test]
-    fn as_scalar() {
-        let value: u32 = Default::default();
-        let dtype = Default::default();
-        let tensor = tf::scalar(&value.into(), dtype);
-        tensor.as_scalar();
-    }
+        #[wasm_bindgen_test]
+        fn clone() {
+            let value = Array::new();
+            let dtype = Default::default();
+            let tensor = tf::tensor1d(&value.into(), dtype);
+            tf::clone(&tensor);
+        }
 
-    #[wasm_bindgen_test]
-    fn buffer() {
-        let shape: &[i32] = &[];
-        let dtype = Default::default();
-        let values = Default::default();
-        tf::buffer(&shape, dtype, values);
-    }
+        #[wasm_bindgen_test]
+        fn complex() {
+            let real = Default::default();
+            let imag = Default::default();
+            tf::complex(real, imag);
+        }
 
-    #[wasm_bindgen_test]
-    fn clone() {
-        let value = Array::new();
-        let dtype = Default::default();
-        let tensor = tf::tensor1d(&value.into(), dtype);
-        tf::clone(&tensor);
-    }
+        #[wasm_bindgen_test]
+        fn eye() {
+            let num_rows = Default::default();
+            let num_columns = Default::default();
+            let batch_shape = Default::default();
+            let dtype = Default::default();
+            tf::eye(num_rows, num_columns, batch_shape, dtype);
+        }
 
-    #[wasm_bindgen_test]
-    fn complex() {
-        let real = Default::default();
-        let imag = Default::default();
-        tf::complex(real, imag);
-    }
+        #[wasm_bindgen_test]
+        fn fill() {
+            let shape: &[i32] = &[2, 2];
+            let value = 4;
+            let dtype = Default::default();
+            tf::fill(shape, value, dtype);
+        }
 
-    #[wasm_bindgen_test]
-    fn eye() {
-        let num_rows = Default::default();
-        let num_columns = Default::default();
-        let batch_shape = Default::default();
-        let dtype = Default::default();
-        tf::eye(num_rows, num_columns, batch_shape, dtype);
-    }
+        #[wasm_bindgen_test]
+        fn imag() {
+            let real = Default::default();
+            let imag = Default::default();
+            let complex = tf::complex(real, imag);
+            tf::imag(&complex);
+        }
 
-    #[wasm_bindgen_test]
-    fn fill() {
-        let shape: &[i32] = &[2, 2];
-        let value = 4;
-        let dtype = Default::default();
-        tf::fill(shape, value, dtype);
-    }
+        #[wasm_bindgen_test]
+        fn linspace() {
+            let start = 0;
+            let stop = 9;
+            let num = 10;
+            tf::linspace(start, stop, num);
+        }
 
-    #[wasm_bindgen_test]
-    fn imag() {
-        let real = Default::default();
-        let imag = Default::default();
-        let complex = tf::complex(real, imag);
-        tf::imag(&complex);
-    }
+        #[wasm_bindgen_test]
+        fn one_hot() {
+            let indices = {
+                let values = {
+                    let res = Array::new();
+                    res.push(&0.into());
+                    res.push(&1.into());
+                    res
+                };
+                let dtype = Some("int32");
+                tf::tensor1d(&values, dtype)
+            };
+            let depth = 3;
+            let on_value = Default::default();
+            let off_value = Default::default();
+            tf::one_hot(&indices, depth, on_value, off_value);
+        }
 
-    #[wasm_bindgen_test]
-    fn linspace() {
-        let start = 0;
-        let stop = 9;
-        let num = 10;
-        tf::linspace(start, stop, num);
-    }
+        #[wasm_bindgen_test]
+        fn ones() {
+            let shape: &[i32] = &[2, 2];
+            let dtype = Default::default();
+            tf::ones(shape, dtype);
+        }
 
-    #[wasm_bindgen_test]
-    fn one_hot() {
-        let indices = {
+        #[wasm_bindgen_test]
+        fn ones_like() {
+            let value = Array::new();
+            let dtype = Default::default();
+            let tensor = tf::tensor1d(&value.into(), dtype);
+            tf::ones_like(&tensor);
+        }
+
+        #[wasm_bindgen_test]
+        fn print() {
+            let value: u32 = Default::default();
+            let dtype = Default::default();
+            let tensor = tf::scalar(&value.into(), dtype);
+            tensor.print(false);
+        }
+
+        #[wasm_bindgen_test]
+        fn range() {
+            let start = 0;
+            let stop = 9;
+            let step = Default::default();
+            let dtype = Default::default();
+            tf::range(start, stop, step, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn real() {
+            let real = Default::default();
+            let imag = Default::default();
+            let complex = tf::complex(real, imag);
+            tf::real(&complex);
+        }
+
+        #[wasm_bindgen_test]
+        fn scalar() {
+            let value: u32 = Default::default();
+            let dtype = Default::default();
+            tf::scalar(&value.into(), dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor() {
+            let values = Array::new();
+            let shape = Default::default();
+            let dtype = Default::default();
+            tf::tensor(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor1d() {
+            let values = Array::new();
+            let dtype = Default::default();
+            tf::tensor1d(&values, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor2d() {
             let values = {
                 let res = Array::new();
-                res.push(&0.into());
                 res.push(&1.into());
+                res.push(&2.into());
+                res.push(&3.into());
+                res.push(&4.into());
                 res
             };
-            let dtype = Some("int32");
-            tf::tensor1d(&values, dtype)
-        };
-        let depth = 3;
-        let on_value = Default::default();
-        let off_value = Default::default();
-        tf::one_hot(&indices, depth, on_value, off_value);
+            let shape: Option<&[i32]> = Some(&[2, 2]);
+            let dtype = Default::default();
+            tf::tensor2d(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor3d() {
+            let values = {
+                let res = Array::new();
+                res.push(&1.into());
+                res.push(&2.into());
+                res.push(&3.into());
+                res.push(&4.into());
+                res
+            };
+            let shape: Option<&[i32]> = Some(&[2, 2, 1]);
+            let dtype = Default::default();
+            tf::tensor3d(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor4d() {
+            let values = {
+                let res = Array::new();
+                res.push(&1.into());
+                res.push(&2.into());
+                res.push(&3.into());
+                res.push(&4.into());
+                res
+            };
+            let shape: Option<&[i32]> = Some(&[1, 2, 2, 1]);
+            let dtype = Default::default();
+            tf::tensor4d(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor5d() {
+            let values = {
+                let res = Array::new();
+                res.push(&1.into());
+                res.push(&2.into());
+                res.push(&3.into());
+                res.push(&4.into());
+                res.push(&5.into());
+                res.push(&6.into());
+                res.push(&7.into());
+                res.push(&8.into());
+                res
+            };
+            let shape: Option<&[i32]> = Some(&[1, 2, 2, 2, 1]);
+            let dtype = Default::default();
+            tf::tensor5d(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn tensor6d() {
+            let values = {
+                let res = Array::new();
+                res.push(&1.into());
+                res.push(&2.into());
+                res.push(&3.into());
+                res.push(&4.into());
+                res.push(&5.into());
+                res.push(&6.into());
+                res.push(&7.into());
+                res.push(&8.into());
+                res
+            };
+            let shape: Option<&[i32]> = Some(&[1, 1, 2, 2, 2, 1]);
+            let dtype = Default::default();
+            tf::tensor6d(&values, shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn truncated_normal() {
+            let shape: &[i32] = &[2, 2];
+            let mean = Default::default();
+            let std_dev = Default::default();
+            let dtype = Default::default();
+            let seed = Default::default();
+            tf::truncated_normal(shape, mean, std_dev, dtype, seed);
+        }
+
+        #[wasm_bindgen_test]
+        fn variable() {
+            let value = Array::new();
+            let dtype = Default::default();
+            let tensor = tf::tensor1d(&value.into(), dtype);
+            let trainable = Default::default();
+            let name = Default::default();
+            let dtype = Default::default();
+            tf::variable(&tensor, trainable, name, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn zeros() {
+            let shape: &[i32] = &[2, 2];
+            let dtype = Default::default();
+            tf::zeros(shape, dtype);
+        }
+
+        #[wasm_bindgen_test]
+        fn zeros_like() {
+            let tensor = helper::tensor();
+            tf::zeros_like(&tensor);
+        }
     }
 
-    #[wasm_bindgen_test]
-    fn ones() {
-        let shape: &[i32] = &[2, 2];
-        let dtype = Default::default();
-        tf::ones(shape, dtype);
-    }
+    mod methods {
+        use js_sys::Array;
+        use tfjs_sys as tf;
+        use wasm_bindgen_test::*;
 
-    #[wasm_bindgen_test]
-    fn ones_like() {
-        let value = Array::new();
-        let dtype = Default::default();
-        let tensor = tf::tensor1d(&value.into(), dtype);
-        tf::ones_like(&tensor);
-    }
+        #[wasm_bindgen_test]
+        fn as_1d() {
+            let value = Array::new();
+            let dtype = Default::default();
+            let tensor = tf::tensor1d(&value.into(), dtype);
+            tensor.as_1d();
+        }
 
-    #[wasm_bindgen_test]
-    fn print() {
-        let value: u32 = Default::default();
-        let dtype = Default::default();
-        let tensor = tf::scalar(&value.into(), dtype);
-        tensor.print(false);
-    }
-
-    #[wasm_bindgen_test]
-    fn range() {
-        let start = 0;
-        let stop = 9;
-        let step = Default::default();
-        let dtype = Default::default();
-        tf::range(start, stop, step, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn real() {
-        let real = Default::default();
-        let imag = Default::default();
-        let complex = tf::complex(real, imag);
-        tf::real(&complex);
-    }
-
-    #[wasm_bindgen_test]
-    fn scalar() {
-        let value: u32 = Default::default();
-        let dtype = Default::default();
-        tf::scalar(&value.into(), dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor() {
-        let values = Array::new();
-        let shape = Default::default();
-        let dtype = Default::default();
-        tf::tensor(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor1d() {
-        let values = Array::new();
-        let dtype = Default::default();
-        tf::tensor1d(&values, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor2d() {
-        let values = {
-            let res = Array::new();
-            res.push(&1.into());
-            res.push(&2.into());
-            res.push(&3.into());
-            res.push(&4.into());
-            res
-        };
-        let shape: Option<&[i32]> = Some(&[2, 2]);
-        let dtype = Default::default();
-        tf::tensor2d(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor3d() {
-        let values = {
-            let res = Array::new();
-            res.push(&1.into());
-            res.push(&2.into());
-            res.push(&3.into());
-            res.push(&4.into());
-            res
-        };
-        let shape: Option<&[i32]> = Some(&[2, 2, 1]);
-        let dtype = Default::default();
-        tf::tensor3d(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor4d() {
-        let values = {
-            let res = Array::new();
-            res.push(&1.into());
-            res.push(&2.into());
-            res.push(&3.into());
-            res.push(&4.into());
-            res
-        };
-        let shape: Option<&[i32]> = Some(&[1, 2, 2, 1]);
-        let dtype = Default::default();
-        tf::tensor4d(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor5d() {
-        let values = {
-            let res = Array::new();
-            res.push(&1.into());
-            res.push(&2.into());
-            res.push(&3.into());
-            res.push(&4.into());
-            res.push(&5.into());
-            res.push(&6.into());
-            res.push(&7.into());
-            res.push(&8.into());
-            res
-        };
-        let shape: Option<&[i32]> = Some(&[1, 2, 2, 2, 1]);
-        let dtype = Default::default();
-        tf::tensor5d(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn tensor6d() {
-        let values = {
-            let res = Array::new();
-            res.push(&1.into());
-            res.push(&2.into());
-            res.push(&3.into());
-            res.push(&4.into());
-            res.push(&5.into());
-            res.push(&6.into());
-            res.push(&7.into());
-            res.push(&8.into());
-            res
-        };
-        let shape: Option<&[i32]> = Some(&[1, 1, 2, 2, 2, 1]);
-        let dtype = Default::default();
-        tf::tensor6d(&values, shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn truncated_normal() {
-        let shape: &[i32] = &[2, 2];
-        let mean = Default::default();
-        let std_dev = Default::default();
-        let dtype = Default::default();
-        let seed = Default::default();
-        tf::truncated_normal(shape, mean, std_dev, dtype, seed);
-    }
-
-    #[wasm_bindgen_test]
-    fn variable() {
-        let value = Array::new();
-        let dtype = Default::default();
-        let tensor = tf::tensor1d(&value.into(), dtype);
-        let trainable = Default::default();
-        let name = Default::default();
-        let dtype = Default::default();
-        tf::variable(&tensor, trainable, name, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn zeros() {
-        let shape: &[i32] = &[2, 2];
-        let dtype = Default::default();
-        tf::zeros(shape, dtype);
-    }
-
-    #[wasm_bindgen_test]
-    fn zeros_like() {
-        let tensor = super::helper::tensor();
-        tf::zeros_like(&tensor);
+        #[wasm_bindgen_test]
+        fn as_scalar() {
+            let value: u32 = Default::default();
+            let dtype = Default::default();
+            let tensor = tf::scalar(&value.into(), dtype);
+            tensor.as_scalar();
+        }
     }
 }
 
